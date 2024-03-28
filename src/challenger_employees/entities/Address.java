5c12792d7257550ac60eb1c1f7684a6fb2bec1ab
IfDevfernando <ifsoftfernando@gmail.com>
2024-03-28 20:56:52 -0300

    addEmployee,removeEmployee

---
+++ src/challenger_employees/entities/Address.java
@@ -0,0 +1,43 @@
+package challenger_employees.entities;
+
+public class Address {
+	
+	private String email;
+	private String prhone;
+	
+	
+	public Address(String email, String prhone) {
+		
+		this.email = email;
+		this.prhone = prhone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPrhone() {
+		return prhone;
+	}
+
+
+	public void setPrhone(String prhone) {
+		this.prhone = prhone;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Address [email=" + email + ", prhone=" + prhone + "]";
+	}
+	
+	
+
+}
---
+++ src/challenger_employees/entities/Department.java
@@ -0,0 +1,58 @@
+package challenger_employees.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+	
+	private String name;
+	private int payDay;
+	
+	List<Employee> employees = new ArrayList<>();
+	
+	
+	
+	public Department(String name, int payDay) {
+		
+		this.name = name;
+		this.payDay = payDay;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getPayDay() {
+		return payDay;
+	}
+
+
+
+	public void setPayDay(int payDay) {
+		this.payDay = payDay;
+	}
+	
+	public void addEmployee(Employee employee) {
+		
+		employees.add(employee);
+		
+	}
+	public void removeEmployee(Employee employee) {
+		employees.remove(employee);
+	}
+	public double payroll() {
+		return 0.0;
+	}
+	
+
+}
